4858ba14294169ceaf1861418a1470f4b1637b1e
Jin An <jin.an@dtcc.edu>
2023-05-24 14:33:26 -0400

    Create assignment_template.cpp

---
+++ assignment_template.cpp
@@ -0,0 +1,17 @@
+/*
+Name:
+Course:
+Section:
+Date:
+Assignment:
+
+I wrote all the code submitted OR I have provided citations and references where appropriate
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	return 0;
+}
